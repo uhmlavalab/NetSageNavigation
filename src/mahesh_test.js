fd67b7998dca60ddf6889ab73fd14160505cd0e1
Mahesh Khanal <mkhanal@hawaii.edu>
2019-02-05 00:47:51 -1000

    Updated editable hamburger logo
    Hamburger logo can now be editable.
    Refresh website to see the changes.

--- src/mahesh_test.js
+++ src/mahesh_test.js
@@ -20,10 +20,10 @@ limitations under the License.
 */
 
 
-import {MetricsPanelCtrl} from 'app/plugins/sdk';
+import { MetricsPanelCtrl } from 'app/plugins/sdk';
 import _ from 'lodash';
-import {Scale} from './scale';
-import {CustomHover} from './CustomHover';
+import { Scale } from './scale';
+import { CustomHover } from './CustomHover';
 import './css/maheshtest_styles.css!';
 
 
@@ -36,46 +36,46 @@ import './css/maheshtest_styles.css!';
 
 ////// place global variables here ////
 const panelDefaults = {
-    option_1: "netsage",
-    option_2: "maheshtest",
-    option_3: "plugin",
-    option_4: 123,
-    choices: [],
-    array_option_1: [],
-    array_option_2: [],
-    array_option_3: [],
-    array_option_4: [],
-    parsed_data: [],
-    max_total: 0,
-    to_Byte: false,
-    color: {
-        mode: 'spectrum',
-        cardColor: '#b4ff00',
-        colorScale: 'linear',
-        exponent: 0.5,
-        colorScheme: 'interpolateOranges',
-        fillBackground: false
-    },
-    legend: {
-        show: true,    
-        legend_colors: []
-    },
-    tooltip:{
-        show: true,
-        showDefault: true,
-        content: ' '
-    },
-    to_si: 1000000000,
-    scales: ['linear', 'sqrt'],
-    colorScheme : 'NetSage',
-    rgb_values:[],
-    hex_values:[],
-    //colorModes : ['opacity','spectrum'],
-    colorModes : ['spectrum'],
-    custom_hover: ' '
+  option_1: "netsage",
+  option_2: "maheshtest",
+  option_3: "plugin",
+  option_4: 123,
+  choices: [],
+  array_option_1: [],
+  array_option_2: [],
+  array_option_3: [],
+  array_option_4: [],
+  parsed_data: [],
+  max_total: 0,
+  to_Byte: false,
+  color: {
+    mode: 'spectrum',
+    cardColor: '#b4ff00',
+    colorScale: 'linear',
+    exponent: 0.5,
+    colorScheme: 'interpolateOranges',
+    fillBackground: false
+  },
+  legend: {
+    show: true,
+    legend_colors: []
+  },
+  tooltip: {
+    show: true,
+    showDefault: true,
+    content: ' '
+  },
+  to_si: 1000000000,
+  scales: ['linear', 'sqrt'],
+  colorScheme: 'NetSage',
+  rgb_values: [],
+  hex_values: [],
+  //colorModes : ['opacity','spectrum'],
+  colorModes: ['spectrum'],
+  custom_hover: ' '
 };
 
-var tempArray=[];
+var tempArray = [];
 
 
 export class MaheshTest extends MetricsPanelCtrl {
@@ -84,31 +84,32 @@ export class MaheshTest extends MetricsPanelCtrl {
 
   constructor($scope, $injector) {
     super($scope, $injector);
-    
+
     _.defaults(this.panel, panelDefaults);
-      this.maheshtest_holder_id = 'maheshtest_' + this.panel.id;
-      this.containerDivId = 'container_'+this.maheshtest_holder_id;
-      this.custom_hover = new CustomHover(this.panel.tooltip.content);
-      this.scale = new Scale(this.colorScheme);
-      this.colorSchemes=this.scale.getColorSchemes();
-      this.events.on('data-received', this.onDataReceived.bind(this));
-      this.events.on('data-error', this.onDataError.bind(this));
-      this.events.on('data-snapshot-load', this.onDataReceived.bind(this));
-      this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
-      this.events.on('init-panel-actions', this.onInitPanelActions.bind(this));
+    this.maheshtest_holder_id = 'maheshtest_' + this.panel.id;
+    this.containerDivId = 'container_' + this.maheshtest_holder_id;
+    this.custom_hover = new CustomHover(this.panel.tooltip.content);
+    this.scale = new Scale(this.colorScheme);
+    this.colorSchemes = this.scale.getColorSchemes();
+    this.events.on('data-received', this.onDataReceived.bind(this));
+    this.events.on('data-error', this.onDataError.bind(this));
+    this.events.on('data-snapshot-load', this.onDataReceived.bind(this));
+    this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
+    this.events.on('init-panel-actions', this.onInitPanelActions.bind(this));
   }
 
 
 
-  onDataReceived(dataList) { this.panel.parsed_data = [];
+  onDataReceived(dataList) {
+    this.panel.parsed_data = [];
     this.panel.max_total = 0;
     this.process_data(dataList);
     this.render();
   }
-    
 
 
-  process_data(dataList){
+
+  process_data(dataList) {
     /////////////////////////////////// IMPORTANT NOTE //////////////////////////////////////
     //    datalist == results
     //    each results have (target, datapoints[])
@@ -116,65 +117,65 @@ export class MaheshTest extends MetricsPanelCtrl {
     //
     //    multiple queries get returned as one result.
     /////////////////////////////////// IMPORTANT NOTE //////////////////////////////////////
-   
+
 
     ///////// your data processing code here /////////////
     var self = this;
     //update with the data!
-    _.forEach(dataList, function(data){
-        var min;
-        var max;
-        var avg = 0;
-        var avg_count = 0;
-        var total;
-        var interval;
-        var label;
+    _.forEach(dataList, function (data) {
+      var min;
+      var max;
+      var avg = 0;
+      var avg_count = 0;
+      var total;
+      var interval;
+      var label;
 
-        //get node label
-        label = data.target;
+      //get node label
+      label = data.target;
 
-        //find the last valid value
-        //find the min
-        //find the max
-        //find the average
-        //find the total datapoints
-        for (var i = (data.datapoints.length - 1); i >= 0; i--){
-          var value = data.datapoints[i][0];
-          if(value !== undefined && value !== null){
-            avg += value;
-            avg_count += 1;
-            if(min === undefined){
-              min = value;
-              max = value;
-            }
-            if(value < min){
-              min = value;
-            }
-            if(value > max){
-              max = value;
-            }
+      //find the last valid value
+      //find the min
+      //find the max
+      //find the average
+      //find the total datapoints
+      for (var i = (data.datapoints.length - 1); i >= 0; i--) {
+        var value = data.datapoints[i][0];
+        if (value !== undefined && value !== null) {
+          avg += value;
+          avg_count += 1;
+          if (min === undefined) {
+            min = value;
+            max = value;
+          }
+          if (value < min) {
+            min = value;
+          }
+          if (value > max) {
+            max = value;
           }
         }
+      }
 
-        //get total duration of data set
-        if(total > 1){
-          var start = data.datapoints[0][1];
-          var end = data.datapoints[1][1];
-          interval = end - start;
-        }
+      //get total duration of data set
+      if (total > 1) {
+        var start = data.datapoints[0][1];
+        var end = data.datapoints[1][1];
+        interval = end - start;
+      }
 
       //put all data into object then into array 
       self.panel.parsed_data.push(
         {
-          "min" : min,
-          "max" : max,
-          "avg" : avg/avg_count,
-          "total" : avg,
-          "interval" : interval,
-          "label" : label
+          "min": min,
+          "max": max,
+          "avg": avg / avg_count,
+          "total": avg,
+          "interval": interval,
+          "label": label
         }
       );
-      if(avg > self.panel.max_total){
+      if (avg > self.panel.max_total) {
         self.panel.max_total = avg;
       }
 
@@ -183,7 +184,7 @@ export class MaheshTest extends MetricsPanelCtrl {
   }
 
 
-    
+
   onDataError(err) {
     this.dataRaw = [];
   }
@@ -194,10 +195,10 @@ export class MaheshTest extends MetricsPanelCtrl {
     this.addEditorTab('Options', 'public/plugins/mahesh-test/editor.html', 2);
     this.addEditorTab('Display', 'public/plugins/mahesh-test/display_editor.html', 3);
     this.addEditorTab('Navigation', 'public/plugins/mahesh-test/nav_editor.html', 3);
-    tempArray=this.scale.displayColor(this.panel.colorScheme);
+    tempArray = this.scale.displayColor(this.panel.colorScheme);
     this.render();
-  }  
-   
+  }
+
 
 
   onInitPanelActions(actions) {
@@ -216,23 +217,23 @@ export class MaheshTest extends MetricsPanelCtrl {
 
 
   removeChoice(index) {
-    this.panel.choices.splice(index,1);
-    this.panel.array_option_1.splice(index,1);
-    this.panel.array_option_2.splice(index,1);
-    this.panel.array_option_3.splice(index,1);
-    this.panel.array_option_4.splice(index,1);
+    this.panel.choices.splice(index, 1);
+    this.panel.array_option_1.splice(index, 1);
+    this.panel.array_option_2.splice(index, 1);
+    this.panel.array_option_3.splice(index, 1);
+    this.panel.array_option_4.splice(index, 1);
   }
 
 
   display() {
-    this.panel.colors=this.scale.displayColor(this.panel.colorScheme);
+    this.panel.colors = this.scale.displayColor(this.panel.colorScheme);
     this.panel.rgb_values = this.panel.colors.rgb_values;
     this.panel.hex_values = this.panel.colors.hex_values;
   }
 
 
 
-  getHtml(htmlContent){
+  getHtml(htmlContent) {
     return this.custom_hover.parseHtml(htmlContent);
     ///use in link///
     //             let html_content = ctrl.getHtml(ctrl.panel.tooltip.content);
@@ -242,30 +243,30 @@ export class MaheshTest extends MetricsPanelCtrl {
   formatBytes(val) {
     var hrFormat = null;
     var factor = 1024.0
-    val = val/8.0;
-    
-    var b = val;
-    var k = val/factor;
-    var m = ((val/factor)/factor);
-    var g = (((val/factor)/factor)/factor);
-    var t = ((((val/factor)/factor)/factor)/factor);
-    var p = (((((val/factor)/factor)/factor)/factor)/factor);
+    val = val / 8.0;
 
-    if ( p>1 ) {
-        hrFormat = p.toFixed(2)+"(PB)";
-    } else if ( t>1 ) {
-        hrFormat = t.toFixed(2)+"(TB)";
-    } else if ( g>1 ) {
-        hrFormat = g.toFixed(2)+"(GB)";
-    } else if ( m>1 ) {
-        hrFormat = m.toFixed(2)+"(MB)";
-    } else if ( k>1 ) {
-        hrFormat = k.toFixed(2)+"(KB)";
+    var b = val;
+    var k = val / factor;
+    var m = ((val / factor) / factor);
+    var g = (((val / factor) / factor) / factor);
+    var t = ((((val / factor) / factor) / factor) / factor);
+    var p = (((((val / factor) / factor) / factor) / factor) / factor);
+
+    if (p > 1) {
+      hrFormat = p.toFixed(2) + "(PB)";
+    } else if (t > 1) {
+      hrFormat = t.toFixed(2) + "(TB)";
+    } else if (g > 1) {
+      hrFormat = g.toFixed(2) + "(GB)";
+    } else if (m > 1) {
+      hrFormat = m.toFixed(2) + "(MB)";
+    } else if (k > 1) {
+      hrFormat = k.toFixed(2) + "(KB)";
     } else {
-        hrFormat = b.toFixed(2)+"(Bytes)";
+      hrFormat = b.toFixed(2) + "(Bytes)";
     }
 
-      return hrFormat
+    return hrFormat
   }
 
   formatBits(val) {
@@ -273,120 +274,116 @@ export class MaheshTest extends MetricsPanelCtrl {
     var factor = 1024.0
 
     var b = val;
-    var k = val/factor;
-    var m = ((val/factor)/factor);
-    var g = (((val/factor)/factor)/factor);
-    var t = ((((val/factor)/factor)/factor)/factor);
-    var p = (((((val/factor)/factor)/factor)/factor)/factor);
+    var k = val / factor;
+    var m = ((val / factor) / factor);
+    var g = (((val / factor) / factor) / factor);
+    var t = ((((val / factor) / factor) / factor) / factor);
+    var p = (((((val / factor) / factor) / factor) / factor) / factor);
 
-    if ( p>1 ) {
-        hrFormat = p.toFixed(2)+"(Pb)";
-    } else if ( t>1 ) {
-        hrFormat = t.toFixed(2)+"(Tb)";
-    } else if ( g>1 ) {
-        hrFormat = g.toFixed(2)+"(Gb)";
-    } else if ( m>1 ) {
-        hrFormat = m.toFixed(2)+"(Mb)";
-    } else if ( k>1 ) {
-        hrFormat = k.toFixed(2)+"(Kb)";
+    if (p > 1) {
+      hrFormat = p.toFixed(2) + "(Pb)";
+    } else if (t > 1) {
+      hrFormat = t.toFixed(2) + "(Tb)";
+    } else if (g > 1) {
+      hrFormat = g.toFixed(2) + "(Gb)";
+    } else if (m > 1) {
+      hrFormat = m.toFixed(2) + "(Mb)";
+    } else if (k > 1) {
+      hrFormat = k.toFixed(2) + "(Kb)";
     } else {
-        hrFormat = b.toFixed(2)+"(bits)";
+      hrFormat = b.toFixed(2) + "(bits)";
     }
 
-      return hrFormat
+    return hrFormat
   }
 
 
-  navbarOpen(){
+  navbarOpen() {
     document.getElementById("mySidenav").style.width = "420px";
   }
 
-  navbarClose(){
+  navbarClose() {
     document.getElementById("mySidenav").style.width = "0px";
   }
 
 
-  link(scope, elem, attrs, ctrl){
+  link(scope, elem, attrs, ctrl) {
     var self = this;
-    ctrl.events.on('render', function() {
-        if(document.getElementsByTagName("body")){
-            var navItem = document.getElementById("NavBar"); 
-  //Only create if nav doesnt exist.
-            if(!navItem){
-                //This sets the logos 
-                var navBarLogoPath = ctrl.panel.topLogoPath; 
-                var sideNavLogoPath = ctrl.panel.sideLogoPath;
-
-                
-                CreateSideNavBar(navBarLogoPath,sideNavLogoPath); 
-                CreateTopNavBar(); 
-              }
-	      }
-	      if(document.getElementById(ctrl.maheshtest_holder_id)){
+    ctrl.events.on('render', function () {
+      if (document.getElementsByTagName("body")) {
+        var navItem = document.getElementById("NavBar");
+        //Only create if nav doesnt exist.
+        if (!navItem) {
+          //This sets the logos 
+          var navBarLogoPath = ctrl.panel.topLogoPath;
+          var sideNavLogoPath = ctrl.panel.sideLogoPath;
+          var hamburgerPath = ctrl.panel.hamburgerPath;
+          CreateSideNavBar(navBarLogoPath, sideNavLogoPath, hamburgerPath);
+          CreateTopNavBar();
+        }
+      }
+      if (document.getElementById(ctrl.maheshtest_holder_id)) {
         ctrl.display();
 
         RemoveGrafanaNav();
 
         /////////////////  YOUR CODE HERE //////////////
-       // var htmlContent = '<iframe style= "width:100% ; height:500px; border-width : 0px; " src= "http://tokelau.manoa.hawaii.edu:3000/public/plugins/mahesh-test/src/testfile.html"></iframe>';       
-       // document.getElementById(ctrl.containerDivId).innerHTML = htmlContent;
+        // var htmlContent = '<iframe style= "width:100% ; height:500px; border-width : 0px; " src= "http://tokelau.manoa.hawaii.edu:3000/public/plugins/mahesh-test/src/testfile.html"></iframe>';       
+        // document.getElementById(ctrl.containerDivId).innerHTML = htmlContent;
 
-       //Need to clear the navbar first. 
-       var currNavBar = window.parent.document.getElementById('menuItems'); 
-       currNavBar.innerHTML = "";
-       
-       var questions = [];
-       
-       if(ctrl.panel.array_option_1.length > 0){
-         for(var i = 0; i < ctrl.panel.array_option_1.length; i++){
-           var quesObj = {
-             "Question": ctrl.panel.array_option_1[i], 
-             "Url":ctrl.panel.array_option_2[i]
-           }
-           questions.push(quesObj);
-         }
-       }
+        //Need to clear the navbar first. 
+        var currNavBar = window.parent.document.getElementById('menuItems');
+        currNavBar.innerHTML = "";
 
-    _.forEach(questions, function(data){
-      var aTag = document.createElement('a');
-            aTag.setAttribute('href',data.Url);
-            aTag.innerHTML = data.Question;
-            currNavBar.appendChild(aTag);
-    })
+        var questions = [];
 
-  
+        if (ctrl.panel.array_option_1.length > 0) {
+          for (var i = 0; i < ctrl.panel.array_option_1.length; i++) {
+            var quesObj = {
+              "Question": ctrl.panel.array_option_1[i],
+              "Url": ctrl.panel.array_option_2[i]
+            }
+            questions.push(quesObj);
+          }
+        }
 
-
-
+        _.forEach(questions, function (data) {
+          var aTag = document.createElement('a');
+          aTag.setAttribute('href', data.Url);
+          aTag.innerHTML = data.Question;
+          aTag.onclick = function(){
+            document.getElementById("mySidenav").style.width = "0px";
+          }
+          currNavBar.appendChild(aTag);
+        })
 
 
-    
-    /////////////////  YOUR CODE HERE //////////////
+
       }
     });
   }
-    
+
 }
 
 
-function RemoveGrafanaNav(){
-  document.getElementsByTagName('sidemenu')[0].style.display = 'none';        
+function RemoveGrafanaNav() {
+  document.getElementsByTagName('sidemenu')[0].style.display = 'none';
 }
 
-function CreateTopNavBar(){
+function CreateTopNavBar() {
   var grafanaApp = document.getElementsByTagName("grafana-app")[0];
-  var menuDiv = document.createElement("div"); 
-  menuDiv.setAttribute("id","NavBar");
-  grafanaApp.setAttribute("style","margin-top:50px;"); 
-  document.getElementsByTagName("body")[0].insertBefore(menuDiv,grafanaApp)
+  var menuDiv = document.createElement("div");
+  menuDiv.setAttribute("id", "NavBar");
+  grafanaApp.setAttribute("style", "margin-top:50px;");
+  document.getElementsByTagName("body")[0].insertBefore(menuDiv, grafanaApp)
 
 }
 
-function CreateSideNavBar(toppath, sidepath){
+function CreateSideNavBar(toppath, sidepath, hamburgerPath) {
   console.log(toppath, sidepath);
   var sideBarHtml = '<div id="mySidenav" class="sidenav"><div style="background-color:#ffffff;">'
   sideBarHtml += '<left style = "padding-left:30px;"><img src="'
-  sideBarHtml += sidepath +'" ></left>'
+  sideBarHtml += sidepath + '" ></left>'
   sideBarHtml += '</div><a href="javascript:void(0)" class="closebtn" '
   sideBarHtml += 'onclick="document.getElementById('
   sideBarHtml += "'mySidenav').style.width = "
@@ -396,15 +393,14 @@ function CreateSideNavBar(toppath, sidepath){
   sideBarHtml += 'onclick="document.getElementById('
   sideBarHtml += "'mySidenav').style.width = "
   sideBarHtml += "'420px'"
-  sideBarHtml += ';";"><img src = "http://tokelau.manoa.hawaii.edu:3000/public/plugins/mahesh-test/src/images/hamburger-v4.gif" style = "width:45px;"> <img src="'+toppath+'" style= "width:200px; "></span>'
+  sideBarHtml += ';";"><img src = "' + hamburgerPath + '" style = "width:45px;"> <img src="' + toppath + '" style= "width:200px; "></span>'
   var sideDiv = document.createElement("div");
-  sideDiv.setAttribute("id","SideDiv");
-  sideDiv.innerHTML = sideBarHtml; 
- 
-  var grafanaApp =  document.getElementsByTagName("grafana-app")[0];
-  document.getElementsByTagName("body")[0].insertBefore(sideDiv,grafanaApp)
-
+  sideDiv.setAttribute("id", "SideDiv");
+  sideDiv.innerHTML = sideBarHtml;
+  var grafanaApp = document.getElementsByTagName("grafana-app")[0];
+  document.getElementsByTagName("body")[0].insertBefore(sideDiv, grafanaApp)
 }
+
 
 
 
